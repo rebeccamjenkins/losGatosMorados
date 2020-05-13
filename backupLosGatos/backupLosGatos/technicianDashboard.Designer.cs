096f0fae3e925d091bc4d5f4106dfc97f998395a
Rebecca Jenkins <rebeccamjenkins@ksu.edu>
2020-05-12 21:39:10 -0500

    tech koch logo

--- backupLosGatos/backupLosGatos/technicianDashboard.Designer.cs
+++ backupLosGatos/backupLosGatos/technicianDashboard.Designer.cs
@@ -29,6 +29,7 @@ namespace backupLosGatos
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(technicianDashboard));
             this.equipmentDescriptionLabel = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.title = new System.Windows.Forms.Label();
@@ -77,28 +78,27 @@ namespace backupLosGatos
             // 
             this.equipmentDescriptionLabel.AutoSize = true;
             this.equipmentDescriptionLabel.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.equipmentDescriptionLabel.Location = new System.Drawing.Point(17, 56);
+            this.equipmentDescriptionLabel.Location = new System.Drawing.Point(23, 69);
+            this.equipmentDescriptionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.equipmentDescriptionLabel.Name = "equipmentDescriptionLabel";
-            this.equipmentDescriptionLabel.Size = new System.Drawing.Size(70, 16);
+            this.equipmentDescriptionLabel.Size = new System.Drawing.Size(89, 19);
             this.equipmentDescriptionLabel.TabIndex = 28;
             this.equipmentDescriptionLabel.Text = "Equipment:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(58, 77);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(77, 95);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.Size = new System.Drawing.Size(0, 17);
             this.label2.TabIndex = 15;
             // 
             // title
             // 
             this.title.Font = new System.Drawing.Font("Century Gothic", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.title.Location = new System.Drawing.Point(273, 37);
-            this.title.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.title.Location = new System.Drawing.Point(364, 46);
             this.title.Name = "title";
-            this.title.Size = new System.Drawing.Size(334, 40);
+            this.title.Size = new System.Drawing.Size(445, 49);
             this.title.TabIndex = 14;
             this.title.Text = "Weld Progress Tracking System";
             // 
@@ -112,12 +112,12 @@ namespace backupLosGatos
             this.dateSubmitted,
             this.status});
             this.dashboardGrid.DataSource = this.ticketsBindingSource1;
-            this.dashboardGrid.Location = new System.Drawing.Point(17, 190);
-            this.dashboardGrid.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dashboardGrid.Location = new System.Drawing.Point(23, 234);
+            this.dashboardGrid.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dashboardGrid.Name = "dashboardGrid";
             this.dashboardGrid.RowHeadersWidth = 51;
             this.dashboardGrid.RowTemplate.Height = 24;
-            this.dashboardGrid.Size = new System.Drawing.Size(590, 225);
+            this.dashboardGrid.Size = new System.Drawing.Size(787, 277);
             this.dashboardGrid.TabIndex = 13;
             // 
             // ticketIDDataGridViewTextBoxColumn
@@ -174,9 +174,11 @@ namespace backupLosGatos
             // 
             // Koch
             // 
-            this.Koch.Location = new System.Drawing.Point(17, 36);
+            this.Koch.Image = ((System.Drawing.Image)(resources.GetObject("Koch.Image")));
+            this.Koch.Location = new System.Drawing.Point(23, 44);
+            this.Koch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Koch.Name = "Koch";
-            this.Koch.Size = new System.Drawing.Size(238, 41);
+            this.Koch.Size = new System.Drawing.Size(317, 50);
             this.Koch.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.Koch.TabIndex = 27;
             this.Koch.TabStop = false;
@@ -184,9 +186,10 @@ namespace backupLosGatos
             // labelRole
             // 
             this.labelRole.AutoSize = true;
-            this.labelRole.Location = new System.Drawing.Point(620, 9);
+            this.labelRole.Location = new System.Drawing.Point(827, 11);
+            this.labelRole.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelRole.Name = "labelRole";
-            this.labelRole.Size = new System.Drawing.Size(56, 13);
+            this.labelRole.Size = new System.Drawing.Size(72, 17);
             this.labelRole.TabIndex = 30;
             this.labelRole.Text = "technician";
             this.labelRole.Visible = false;
@@ -201,14 +204,14 @@ namespace backupLosGatos
             this.pageOptions.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
             this.pageOptions.Location = new System.Drawing.Point(0, 0);
             this.pageOptions.Name = "pageOptions";
-            this.pageOptions.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
-            this.pageOptions.Size = new System.Drawing.Size(632, 25);
+            this.pageOptions.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.pageOptions.Size = new System.Drawing.Size(843, 28);
             this.pageOptions.TabIndex = 31;
             // 
             // dashboardPage
             // 
             this.dashboardPage.Name = "dashboardPage";
-            this.dashboardPage.Size = new System.Drawing.Size(63, 21);
+            this.dashboardPage.Size = new System.Drawing.Size(77, 24);
             this.dashboardPage.Text = "Refresh";
             this.dashboardPage.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.dashboardPage.Click += new System.EventHandler(this.dashboardPage_Click);
@@ -216,7 +219,7 @@ namespace backupLosGatos
             // logoutToolStripMenuItem
             // 
             this.logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
-            this.logoutToolStripMenuItem.Size = new System.Drawing.Size(62, 21);
+            this.logoutToolStripMenuItem.Size = new System.Drawing.Size(74, 24);
             this.logoutToolStripMenuItem.Text = "Logout";
             this.logoutToolStripMenuItem.Click += new System.EventHandler(this.logoutToolStripMenuItem_Click);
             // 
@@ -229,10 +232,10 @@ namespace backupLosGatos
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.ticketLabel);
             this.panel1.Controls.Add(this.queryTicket);
-            this.panel1.Location = new System.Drawing.Point(416, 86);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Location = new System.Drawing.Point(555, 106);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(192, 100);
+            this.panel1.Size = new System.Drawing.Size(255, 123);
             this.panel1.TabIndex = 37;
             // 
             // ticketLabel
@@ -240,31 +243,30 @@ namespace backupLosGatos
             this.ticketLabel.AllowDrop = true;
             this.ticketLabel.AutoSize = true;
             this.ticketLabel.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ticketLabel.Location = new System.Drawing.Point(11, 12);
-            this.ticketLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.ticketLabel.Location = new System.Drawing.Point(15, 15);
             this.ticketLabel.Name = "ticketLabel";
-            this.ticketLabel.Size = new System.Drawing.Size(52, 16);
+            this.ticketLabel.Size = new System.Drawing.Size(65, 19);
             this.ticketLabel.TabIndex = 23;
             this.ticketLabel.Text = "Ticket #:";
             // 
             // queryTicket
             // 
             this.queryTicket.AllowDrop = true;
-            this.queryTicket.Location = new System.Drawing.Point(64, 12);
-            this.queryTicket.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.queryTicket.Location = new System.Drawing.Point(85, 15);
+            this.queryTicket.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.queryTicket.Multiline = true;
             this.queryTicket.Name = "queryTicket";
-            this.queryTicket.Size = new System.Drawing.Size(119, 21);
+            this.queryTicket.Size = new System.Drawing.Size(157, 25);
             this.queryTicket.TabIndex = 20;
             // 
             // refreshButton
             // 
             this.refreshButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.refreshButton.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.refreshButton.Location = new System.Drawing.Point(261, 54);
-            this.refreshButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.refreshButton.Location = new System.Drawing.Point(348, 66);
+            this.refreshButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.refreshButton.Name = "refreshButton";
-            this.refreshButton.Size = new System.Drawing.Size(118, 21);
+            this.refreshButton.Size = new System.Drawing.Size(157, 26);
             this.refreshButton.TabIndex = 31;
             this.refreshButton.Text = "Reset";
             this.refreshButton.UseVisualStyleBackColor = true;
@@ -274,9 +276,10 @@ namespace backupLosGatos
             this.equipmentOption.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
             this.equipmentOption.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
             this.equipmentOption.FormattingEnabled = true;
-            this.equipmentOption.Location = new System.Drawing.Point(90, 56);
+            this.equipmentOption.Location = new System.Drawing.Point(120, 69);
+            this.equipmentOption.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.equipmentOption.Name = "equipmentOption";
-            this.equipmentOption.Size = new System.Drawing.Size(119, 21);
+            this.equipmentOption.Size = new System.Drawing.Size(157, 24);
             this.equipmentOption.TabIndex = 29;
             // 
             // technicianOption
@@ -285,10 +288,10 @@ namespace backupLosGatos
             this.technicianOption.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
             this.technicianOption.DisplayMember = "associateID";
             this.technicianOption.FormattingEnabled = true;
-            this.technicianOption.Location = new System.Drawing.Point(90, 13);
-            this.technicianOption.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.technicianOption.Location = new System.Drawing.Point(120, 16);
+            this.technicianOption.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.technicianOption.Name = "technicianOption";
-            this.technicianOption.Size = new System.Drawing.Size(119, 21);
+            this.technicianOption.Size = new System.Drawing.Size(157, 24);
             this.technicianOption.TabIndex = 18;
             this.technicianOption.ValueMember = "associateID";
             // 
@@ -298,10 +301,10 @@ namespace backupLosGatos
             this.statusOption.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
             this.statusOption.DisplayMember = "status";
             this.statusOption.FormattingEnabled = true;
-            this.statusOption.Location = new System.Drawing.Point(261, 13);
-            this.statusOption.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.statusOption.Location = new System.Drawing.Point(348, 16);
+            this.statusOption.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.statusOption.Name = "statusOption";
-            this.statusOption.Size = new System.Drawing.Size(119, 21);
+            this.statusOption.Size = new System.Drawing.Size(157, 24);
             this.statusOption.TabIndex = 19;
             this.statusOption.ValueMember = "status";
             // 
@@ -309,10 +312,9 @@ namespace backupLosGatos
             // 
             this.assignedLabel.AutoSize = true;
             this.assignedLabel.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.assignedLabel.Location = new System.Drawing.Point(15, 13);
-            this.assignedLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.assignedLabel.Location = new System.Drawing.Point(20, 16);
             this.assignedLabel.Name = "assignedLabel";
-            this.assignedLabel.Size = new System.Drawing.Size(72, 16);
+            this.assignedLabel.Size = new System.Drawing.Size(90, 19);
             this.assignedLabel.TabIndex = 21;
             this.assignedLabel.Text = "Assigned To:";
             // 
@@ -320,10 +322,9 @@ namespace backupLosGatos
             // 
             this.statusLabel.AutoSize = true;
             this.statusLabel.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.statusLabel.Location = new System.Drawing.Point(214, 13);
-            this.statusLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.statusLabel.Location = new System.Drawing.Point(285, 16);
             this.statusLabel.Name = "statusLabel";
-            this.statusLabel.Size = new System.Drawing.Size(44, 16);
+            this.statusLabel.Size = new System.Drawing.Size(54, 19);
             this.statusLabel.TabIndex = 22;
             this.statusLabel.Text = "Status:";
             // 
@@ -337,19 +338,19 @@ namespace backupLosGatos
             this.filterPanel.Controls.Add(this.statusOption);
             this.filterPanel.Controls.Add(this.assignedLabel);
             this.filterPanel.Controls.Add(this.statusLabel);
-            this.filterPanel.Location = new System.Drawing.Point(17, 85);
+            this.filterPanel.Location = new System.Drawing.Point(23, 105);
+            this.filterPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.filterPanel.Name = "filterPanel";
-            this.filterPanel.Size = new System.Drawing.Size(394, 100);
+            this.filterPanel.Size = new System.Drawing.Size(525, 123);
             this.filterPanel.TabIndex = 36;
             // 
             // techName
             // 
             this.techName.AutoSize = true;
             this.techName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.techName.Location = new System.Drawing.Point(260, 36);
-            this.techName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.techName.Location = new System.Drawing.Point(347, 44);
             this.techName.Name = "techName";
-            this.techName.Size = new System.Drawing.Size(2, 15);
+            this.techName.Size = new System.Drawing.Size(2, 19);
             this.techName.TabIndex = 38;
             this.techName.Visible = false;
             // 
@@ -368,9 +369,9 @@ namespace backupLosGatos
             // 
             // technicianDashboard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(632, 446);
+            this.ClientSize = new System.Drawing.Size(843, 549);
             this.Controls.Add(this.techName);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.filterPanel);
@@ -380,7 +381,7 @@ namespace backupLosGatos
             this.Controls.Add(this.label2);
             this.Controls.Add(this.title);
             this.Controls.Add(this.dashboardGrid);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "technicianDashboard";
             this.Text = "Los Gatos Morados: Weld Progress Tracking System - Technician Dashboard";
             this.Load += new System.EventHandler(this.technicianDashboard_Load);
